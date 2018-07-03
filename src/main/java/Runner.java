3c562b08040ba6b11919df6431c341f93ff82a58
tara <tara.mck@hotmail.com>
2018-07-03 16:50:43 +0100

    tested in debugger

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,3 +1,4 @@
+import db.DBCourse;
 import db.DBHelper;
 import models.Course;
 import models.Level;
@@ -26,9 +27,17 @@ public class Runner {
     Student student3 = new Student("Adri", "Adrison", 28, 567, course4);
     DBHelper.saveOrUpdate(student3);
 
+    student1.setAge(45);
+    DBHelper.saveOrUpdate(student1);
 
+    DBHelper.delete(course5);
+    List<Course> listAfterDelete = DBHelper.getAll(Course.class);
 
     List<Student> studentsAll = DBHelper.getAll(Student.class);
+
+    Course sillyCourse = DBHelper.find(Course.class, 1);
+
+    List<Student> courseStudents = DBCourse.getStudentsForCourse(course4);
 
     }
 }
